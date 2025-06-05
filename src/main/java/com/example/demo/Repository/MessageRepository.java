0476449c91bbb740341d811486d103812e429afb
PraveenRaamC <ckpraveenraam@gmail.com>
2025-06-05 14:10:25 +0530

    Commit Log : Repository were added and connection with SQL

---
+++ src/main/java/com/example/demo/Repository/MessageRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findByUserId(Long id);
+}
